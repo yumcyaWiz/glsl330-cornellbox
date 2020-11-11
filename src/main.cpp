929b233d9b4473eebce8946b918b1f05808e85a1
yumcyawiz <hashdolf1923@gmail.com>
2020-11-12 01:57:13 +0900

    create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,25 @@
-int main() { return 0; }
+#include <iostream>
+
+#include "GLFW/glfw3.h"
+
+int main() {
+  if (!glfwInit()) {
+    std::cerr << "failed to initialize GLFW" << std::endl;
+  }
+
+  glfwSetErrorCallback([](int error, const char* description) {
+    std::cerr << "Error: " << description << std::endl;
+  });
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  GLFWwindow* window =
+      glfwCreateWindow(512, 512, "GLSL CornellBox", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "failed to create window" << std::endl;
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}

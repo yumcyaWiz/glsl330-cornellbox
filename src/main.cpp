ed3c87ad0dd92e50bfa2790c0361d50c61713c45
yumcyawiz <hashdolf1923@gmail.com>
2020-11-13 01:19:21 +0900

    draw rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -50,25 +50,32 @@ int main() {
 
   glViewport(0, 0, 512, 512);
 
-  // setup VBO;
-  GLfloat vertices[] = {1.0f,  -1.0f, 0.0f, -1.0f, -1.0f, 0.0f,
-                        -1.0f, 1.0f,  0.0f, 1.0f,  1.0f,  0.0f};
-  GLuint VBO;
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
   // setup VAO
   GLuint VAO;
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
-  // position
+
+  // setup VBO;
+  GLfloat vertices[] = {-1.0f, -1.0f, 0.0f, 1.0f,  -1.0f, 0.0f,
+                        1.0f,  1.0f,  0.0f, -1.0f, 1.0f,  0.0f};
+  GLuint VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // setup EBO;
+  GLuint indices[] = {0, 1, 2, 2, 3, 0};
+  GLuint EBO;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  // position attribute
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
                         (GLvoid*)0);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
 
   // setup shaders
   Shader shader(vertex_shader_filepath, fragment_shader_filepath);
@@ -81,6 +88,10 @@ int main() {
     glfwPollEvents();
 
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
 
     glfwSwapBuffers(window);
   }

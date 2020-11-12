02f1941646202c1f129d5f634133b25f975c2e77
yumcyawiz <hashdolf1923@gmail.com>
2020-11-13 00:52:23 +0900

    setup VAO

--- src/main.cpp
+++ src/main.cpp
@@ -57,6 +57,18 @@ int main() {
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // setup VAO
+  GLuint VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+  // position
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+                        (GLvoid*)0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
 
   // setup shaders
   Shader shader(vertex_shader_filepath, fragment_shader_filepath);

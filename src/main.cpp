bf753bea1ebbcd1ff03aceea0d74fb808edf33c8
yumcyawiz <hashdolf1923@gmail.com>
2020-12-08 00:32:50 +0900

    use Rectangle on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 //
 #include "glm/glm.hpp"
 //
+#include "rectangle.h"
 #include "shader.h"
 
 const int width = 1024;
@@ -65,32 +66,7 @@ int main() {
     std::exit(EXIT_FAILURE);
   }
 
-  // setup VAO
-  GLuint VAO;
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-
-  // setup VBO;
-  GLfloat vertices[] = {-1.0f, -1.0f, 0.0f, 1.0f,  -1.0f, 0.0f,
-                        1.0f,  1.0f,  0.0f, -1.0f, 1.0f,  0.0f};
-  GLuint VBO;
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  // setup EBO;
-  GLuint indices[] = {0, 1, 2, 2, 3, 0};
-  GLuint EBO;
-  glGenBuffers(1, &EBO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-               GL_STATIC_DRAW);
-
-  // position attribute
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
-                        (GLvoid*)0);
+  Rectangle rectangle;
 
   // setup accumulate FBO
   glGenFramebuffers(1, &accumFBO);
@@ -132,9 +108,7 @@ int main() {
     glBindFramebuffer(GL_FRAMEBUFFER, accumFBO);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, accumTexture);
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
+    rectangle.draw();
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     // output
@@ -145,9 +119,7 @@ int main() {
     output_shader.setUniform("accumTexture", 0);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, accumTexture);
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
+    rectangle.draw();
 
     glfwSwapBuffers(window);
 

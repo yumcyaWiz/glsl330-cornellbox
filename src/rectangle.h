3a4a68b73f0b5d8e278cc6d951d18d02a80598ce
yumcyawiz <hashdolf1923@gmail.com>
2020-12-07 21:32:34 +0900

    fix VAO init

--- src/rectangle.h
+++ src/rectangle.h
@@ -20,7 +20,6 @@ class Rectangle {
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // setup EBO;
     GLuint indices[] = {0, 1, 2, 2, 3, 0};
@@ -29,14 +28,16 @@ class Rectangle {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                  GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     // position attribute
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
                           (GLvoid*)0);
+
+    // unbind VAO, VBO, EBO
+    glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
   }
 
   void draw() const {

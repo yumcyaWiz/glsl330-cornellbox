de25fdd375eddd6f3ad84ec74b206da7f068b147
yumcyawiz <hashdolf1923@gmail.com>
2020-12-19 17:15:22 +0900

    fix Shader::setUBO

--- src/shader.h
+++ src/shader.h
@@ -163,9 +163,11 @@ class Shader {
     deactivate();
   }
 
-  void setUBO(const std::string& block_name, GLuint binding_number) {
-    GLuint index = glGetUniformBlockIndex(program, block_name.c_str());
+  void setUBO(const std::string& block_name, GLuint binding_number) const {
+    activate();
+    const GLuint index = glGetUniformBlockIndex(program, block_name.c_str());
     glUniformBlockBinding(program, index, binding_number);
+    deactivate();
   }
 
   /*

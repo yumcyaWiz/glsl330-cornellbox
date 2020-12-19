2889b7382afd5f45387b90182bc50b9c4a73befc
yumcyawiz <hashdolf1923@gmail.com>
2020-12-19 19:16:22 +0900

    fix for mac

--- src/main.cpp
+++ src/main.cpp
@@ -65,6 +65,7 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // Required on Mac
   GLFWwindow* window =
       glfwCreateWindow(width, height, "GLSL CornellBox", nullptr, nullptr);
   if (!window) {
